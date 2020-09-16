e6546c05ea98fb7dcfc4b14c4793c3693b711d1a
Cole Cantwell <CantCole333@GoTVDSB.ca>
2020-09-16 10:35:31 -0400

    work on part 3

--- Part 3/Program.cs
+++ Part 3/Program.cs
@@ -10,7 +10,29 @@ namespace Part_3
     {
         static void Main(string[] args)
         {
+            //DateTime now = DateTime.Today;
+            //string name;
+            //string greeting = "Hello There ";
+            //int userAge;
+            //int year = now.Year;
 
+            //Console.WriteLine("Please enter your name");
+            //name = Console.ReadLine();
+            //Console.WriteLine($"{greeting}{name}");
+            //;
+            //Console.WriteLine("Please type your age");
+            //userAge = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine(year - userAge);
+            int numOne;
+            int numTwo;
+            int numThree;
+            int sum;
+            Console.WriteLine("type three numbers");
+            numOne = Convert.ToInt32(Console.ReadLine());
+            numTwo = Convert.ToInt32(Console.ReadLine());
+            numThree = Convert.ToInt32(Console.ReadLine());
+            sum = numOne + numTwo + numThree;
+            Console.WriteLine($"The sum is... {sum}");
             Console.ReadLine();
         }
     }
